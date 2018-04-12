1a52141ba987bc05778de9a2634a2d94b8aa8f0d
Jeremy <JeremyBernien@GMAIL.COM>
2018-04-12 12:11:51 -0500

    Add files via upload

--- Main.js
+++ Main.js
@@ -3,7 +3,7 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'monkey.png') {
-      myImage.setAttribute ('src','monkey.png');
+      myImage.setAttribute ('src','puppy.png');
     } else {
       myImage.setAttribute ('src','monkey.png');
     }
